4e5278f93d007b96cc23c3352c199170ee0a812b
shegonde <shegonde@INPUNSHEGONDE.atrapa.deloitte.com>
2023-09-29 15:46:18 +0530

    added upload file functionality

---
+++ src/main/java/com/example/resumeprocessor/ResumeProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.resumeprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeProcessorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/resumeprocessor/controller/WordDocumentController.java
@@ -0,0 +1,46 @@
+package com.example.resumeprocessor.controller;
+
+import java.io.File;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/word")
+public class WordDocumentController {
+	
+	@GetMapping("/status")
+	public String getStatus() {
+		return "Working......";
+	}
+	
+	@PostMapping(value =  "/upload")
+	public ResponseEntity<String> uploadResume(@RequestParam("file") MultipartFile file) {
+		
+		if(!file.isEmpty()) {
+			
+			String fileName = file.getOriginalFilename();
+			
+			try {
+				file.transferTo(new File("C:\\Users\\shegonde\\Documents\\resumes\\"+fileName));
+			}
+			catch (Exception e) {
+			      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		    } 
+			
+		}
+	    
+		return ResponseEntity.ok("File uploaded successfully.");
+		
+	}
+
+}
